ff9854f2588ac2a1ada050762f0f045b96d6a683
presetlaboficial <presetlaboficial@gmail.com>
2026-01-22 23:30:42 -0300

    add loading in checkout

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { combineLatest, map, Observable, take } from 'rxjs';
+import { map, Observable, take } from 'rxjs';
 import { CartService } from '../../core/services/cart.service';
 import { AuthService } from '../../core/services/auth.service';
 import { OrderService } from '../../core/services/order.service';
@@ -21,6 +21,8 @@ export class CheckoutComponent {
   total$!: Observable<number>;
   user$!: Observable<User | null>;
 
+  isLoading = false;
+
   constructor(
     private cartService: CartService,
     private auth: AuthService,
@@ -39,28 +41,39 @@ export class CheckoutComponent {
   }
 
   confirmOrder() {
+    if (this.isLoading) return;
+
+    this.isLoading = true;
+
     this.auth.user$.pipe(take(1)).subscribe((user) => {
-      if (!user) return;
+      if (!user) {
+        this.isLoading = false;
+        return;
+      }
 
       this.cart$.pipe(take(1)).subscribe((items) => {
-        if (!items.length) return;
+        if (!items.length) {
+          this.isLoading = false;
+          return;
+        }
 
-        // payload para o backend
         const payload = {
           userId: user.uid,
           items: items,
         };
 
-        // Chama seu backend local
         this.http
-          .post<{
-            url: string;
-          }>('http://localhost:3000/create-checkout-session', payload)
+          .post<{ url: string }>('http://localhost:3000/create-checkout-session', payload)
           .subscribe({
             next: (response) => {
+              // Redireciona
               window.location.href = response.url;
             },
-            error: (err) => console.error('Erro ao iniciar pagamento', err),
+            error: (err) => {
+              console.error('Erro ao iniciar pagamento', err);
+              this.isLoading = false;
+              alert('Ocorreu um erro ao conectar com o pagamento via Stripe.');
+            },
           });
       });
     });
